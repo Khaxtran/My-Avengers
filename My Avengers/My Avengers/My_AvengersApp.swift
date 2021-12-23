0bf334ae6c018375b71b9ba2605598bb7b506417
Kha Tran <khatran961217@gmail.com>
2021-12-23 16:03:49 +1100

    First commit to master

---
+++ My Avengers/My Avengers/My_AvengersApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_AvengersApp.swift
+//  My Avengers
+//
+//  Created by Kha Tran on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct My_AvengersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
